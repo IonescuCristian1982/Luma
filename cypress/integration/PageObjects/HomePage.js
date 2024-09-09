e6572ac01ddfa663f506ae129ab5bd488470144a
IonescuCristianValentin <ionescu_cristian_valentin@yahoo.com>
2024-09-09 17:43:22 +0300

    Update HomePage.js

--- cypress/integration/PageObjects/HomePage.js
+++ cypress/integration/PageObjects/HomePage.js
@@ -11,8 +11,29 @@ class HomePage {
     selectJacketsSection() {
         return cy.get("#ui-id-19 > span:contains('Jackets')")
     }
+
+    searchInput() {
+        return cy.get('#search')
+    }
+
+    searchButton() {
+        return cy.get(".action.search")
+    }
+
+    searchForProduct(productName) {
+        this.clearSearch()
+        this.searchInput().type(productName)
+        this.searchButton().click()
+    }
+
+    enterSearchText(text) {
+        this.clearSearch()
+        this.searchInput().type(text)
+    }
+
+    clearSearch() {
+        this.searchInput().invoke('val', '').trigger('input')
+    }
 }
 
 export default HomePage;
-
- 
